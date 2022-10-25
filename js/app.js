7445dbbcb80deaae71d46a4af0966933cfd37f0d
antoniochangoer <54006126+antoniochangoer@users.noreply.github.com>
2022-10-25 20:26:00 +0200

    clear props after animation end

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@ const tl = gsap.timeline({
     }
 });
 
-tl.fromTo('header', {y: -500, opacity: 0}, {y: 0, opacity: 1});
-tl.fromTo('.hero-image', {x: 2000}, {x: 0}), '<50%';
-tl.fromTo('.hero-section-content', {x: -2000}, {x: 0}), '<50%';
-tl.fromTo('.course-alert', {opacity: 0, x:-100}, {opacity: 1, x:0}, '>25%');
-tl.fromTo('.course-card', {opacity: 0, x:100}, {opacity: 1, x:0}, '<');
+tl.fromTo('header', {y: -500, opacity: 0}, {y: 0, opacity: 1, clearProps: "all"});
+tl.fromTo('.hero-image', {x: 2000}, {x: 0, clearProps: "all"}), '<50%';
+tl.fromTo('.hero-section-content', {x: -2000}, {x: 0, clearProps: "all"}), '<50%';
+tl.fromTo('.course-alert', {opacity: 0, x:-100}, {opacity: 1, x:0, clearProps: "all"}, '>25%');
+tl.fromTo('.course-card', {opacity: 0, x:100}, {opacity: 1, x:0, clearProps: "all"}, '<');
