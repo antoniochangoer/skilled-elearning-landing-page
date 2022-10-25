56cf831eae963f4301526149841d88bb2384fc82
antoniochangoer <54006126+antoniochangoer@users.noreply.github.com>
2022-10-25 20:46:29 +0200

    uncomment js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-// const tl = gsap.timeline({
-//     defaults: {
-//         duration: 1,
-//         ease: 'power1.out'
-//     }
-// });
+const tl = gsap.timeline({
+    defaults: {
+        duration: 1,
+        ease: 'power1.out'
+    }
+});
 
-// tl.fromTo('header', {y: -500, opacity: 0}, {y: 0, opacity: 1, clearProps: "all"});
-// tl.fromTo('.hero-image', {x: 2000}, {x: 0, clearProps: "all"}), '<50%';
-// tl.fromTo('.hero-section-content', {x: -2000}, {x: 0, clearProps: "all"}), '<50%';
-// tl.fromTo('.course-alert', {opacity: 0, x:-100}, {opacity: 1, x:0, clearProps: "all"}, '>25%');
-// tl.fromTo('.course-card', {opacity: 0, x:100}, {opacity: 1, x:0, clearProps: "all"}, '<');
+tl.fromTo('header', {y: -500, opacity: 0}, {y: 0, opacity: 1, clearProps: "all"});
+tl.fromTo('.hero-image', {x: 2000}, {x: 0, clearProps: "all"}), '<50%';
+tl.fromTo('.hero-section-content', {x: -2000}, {x: 0, clearProps: "all"}), '<50%';
+tl.fromTo('.course-alert', {opacity: 0, x:-100}, {opacity: 1, x:0, clearProps: "all"}, '>25%');
+tl.fromTo('.course-card', {opacity: 0, x:100}, {opacity: 1, x:0, clearProps: "all"}, '<');
